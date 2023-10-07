13cc0db768d8dba6721f98b68ceb6b73b411db3e
bristimaharjan <bristimah@gmail.com>
2023-10-07 18:43:15 +0545

    enum

---
+++ enum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+enum{sunday,monday,tuesday,wednesday,thursday,friday};
+int main()
+{
+  int week;
+  week=monday;
+  cout<<"Monday value :"<<week+1;
+  return 0;
+}
